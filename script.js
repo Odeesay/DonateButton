96f5ac7dd4013da20d1022a8816deecad398a110
Sergiy Labay <85057302+Odeesay@users.noreply.github.com>
2022-05-30 17:34:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// console.log(window.clientInformation.platform);
+
+new ClipboardJS('.monobank__icon');
+
+$('#donation__butt').click(function(){
+    $('#donation__info__screen').slideDown();
+    $('#donation__info__screen').css('display', 'flex');
+    
+})
+
+$('#donation__card-cross').click(function(){
+    $('#donation__info__screen').slideUp()
+})
+
+$('#donation__card__accept-button').click(function(){
+    $('#donation__info__screen').slideUp()
+})
+
+// $('#donation__butt').mousemove(function(){
+//     $('#button__tip').css('display', 'flex')
+// })
+
+// $('#donation__butt').mouseleave(function(){
+//     $('#button__tip').css('display', 'none')
+// })
+
+
+tippy('#donation__butt', {
+    content: "Натисніть, щоб дізнатися більше",
+    animation: 'scale'
+  });
+
+  tippy('#monobank__icon', {
+    content: "Натисніть, щоб скопіювати",
+    animation: 'scale',
+    followCursor: true
+  });
+
+  tippy('#monobank__icon', {
+    content: "Скопійовано",
+    trigger: 'click',
+    animation: 'scale',
+    placement: 'bottom',
+  });
+
+  tippy('#privatbank__icon', {
+    content: "Натисніть, щоб скопіювати",
+    animation: 'scale',
+    followCursor: true
+  });
+
+  tippy('#privatbank__icon', {
+    content: "Скопійовано",
+    trigger: 'click',
+    animation: 'scale',
+    placement: 'bottom',
+  });
